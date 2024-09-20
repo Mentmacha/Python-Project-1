7634a1199ac8228b42811ff82a2a5e994fdc3dc8
@MACHARIA <144136669+Mentmacha@users.noreply.github.com>
2024-09-20 14:53:33 +0300

    Update (lesson 1)Hello.py

--- (lesson 1)Hello.py
+++ (lesson 1)Hello.py
@@ -1,2 +1,13 @@
-greeting='Hello world!'
-print(greeting)
+#asking user to feed information ,Remove the whitespaces &capitalizing
+name=input("What's your name ?").strip().title()
+
+  
+
+print (f"hello,{name}")#formatting my name 
+print(name)
+
+
+
+  #split users name to get one single name
+first,last=name.split(" ")
+print (f"hello,{first}")
