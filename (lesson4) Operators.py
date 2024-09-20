b25e31c933250850178ae471aacd2038c7021fff
@MACHARIA <144136669+Mentmacha@users.noreply.github.com>
2024-09-20 08:06:31 +0300

    Add files via upload

---
+++ (lesson4) Operators.py
@@ -0,0 +1,15 @@
+Number=10
+Number+=1 #Adding a number value  by one
+print(Number)
+Number=11
+Number-=1 #Reducing a number value by two
+print(Number)
+print("Your Answer =", Number)#Concatinating a string and a number
+
+#Checking if numbers are equal
+X=True
+Y=False
+Z=True
+Statement="Are you a good ?"
+print(Statement,X,"OR",Y,)
+
